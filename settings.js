7739b2e61e604e33850d8d59b682b2f4f7757579
shehandanesh84-cmd <shehandanesh84@gmail.com>
2025-09-25 21:28:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === 𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=251545cf315418fe
+ ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
